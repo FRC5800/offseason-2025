f01170821daad986e38f9a2be5a2b71e2d2f44ea
shiimeji <cogomeelo@gmail.com>
2025-08-05 19:58:54 -0300

    elevador ruim lixo

---
+++ src/main/java/frc/robot/subsystems/Elevador.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Elevador extends SubsystemBase {
+
+    private static final int LEAD_ID = 1;
+    private static final int FOLLOWER_ID = 2;
+
+    WPI_TalonSRX elevadorMotorLead = new WPI_TalonSRX(LEAD_ID);
+    WPI_TalonSRX elevadorMotorFollower = new WPI_TalonSRX(FOLLOWER_ID);
+
+    public PIDController pidControllerElevador = new PIDController(0.015, 0.01, 0.005);
+
+    public Elevador() {
+        elevadorMotorFollower.follow(elevadorMotorLead);
+    };
+
+    @Override
+    public void periodic() {
+    // This method will be called once per scheduler run
+    }
+
+    public void levantagem(double controlePos) {
+        elevadorMotorLead.set(controlePos * 0.5);
+    }
+}
+
