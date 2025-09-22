fa02a558a6ad4e589c98245a936805d1d75c281a
nicole <nicknicole.ml@gmail.com>
2025-09-22 19:06:12 -0300

    terminei os comandos do climber e fiz o robot container dele

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,24 +8,26 @@ import frc.robot.subsystems.XDrive;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ClimberComm;
+import frc.robot.subsystems.Climber;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+
 public class RobotContainer {
   private XDrive xDrive = new XDrive();
   private Joystick controller = new Joystick(0);
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  private Climber climber = new Climber();
+ 
   public RobotContainer() {
-    // Configure the trigger bindings
+
     configureBindings();
   }
 
   private void configureBindings() {
     xDrive.setDefaultCommand(new RunCommand(() -> xDrive.drive(-controller.getY(), controller.getX(), controller.getZ()), xDrive));
+    climber.setDefaultCommand(new ClimberComm(climber, controller));
   }
 
   /**
@@ -34,7 +36,7 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
+    
     return null;
   }
 }
--- src/main/java/frc/robot/commands/ClimberComm.java
+++ src/main/java/frc/robot/commands/ClimberComm.java
@@ -4,26 +4,42 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Climber;
+
+
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class ClimberComm extends Command {
+  private Climber climber;
+  Joystick controle;
+  PS4Controller controleBotoes;
+
   /** Creates a new ClimberComm. */
-  public ClimberComm() {
-    // Use addRequirements() here to declare subsystem dependencies.
+  public ClimberComm(Climber climber, Joystick controle) {
+    this.climber = climber;
+    addRequirements(climber);
+    this.controle = controle;
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
-
+ 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    climber.run(-controle.getY());
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    climber.run(0);
+  }
 
   // Returns true when the command should end.
   @Override
