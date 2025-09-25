0b36e9f69a8b2869c4b7b71d924124542b8154e0
anaaafreire <anafreirefamilia@gmail.com>
2025-09-25 19:52:44 -0300

    Created vision system

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,19 +34,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
   private XDrive xDrive = new XDrive();
   private Joystick controller = new Joystick(0);
   private Climber climber = new Climber();
   private Elevador elevador = new Elevador();
   private PivotFunil pivotFunil = new PivotFunil();
-
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
---
+++ src/main/java/frc/robot/subsystems/VisionSystem.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSystem extends SubsystemBase {
+  private AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
+  private Transform3d robotToCam = new Transform3d();
+  private PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, robotToCam);
+  PhotonCamera camera = new PhotonCamera("");
+  Optional<EstimatedRobotPose> pose = Optional.empty();
+
+  /** Creates a new VisionSystem. */
+  public VisionSystem() {}
+
+  public Optional<Pose2d> getEstimatedPose() {
+    return pose.map(p -> p.estimatedPose.toPose2d());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    List<PhotonPipelineResult> results = camera.getAllUnreadResults();
+    for(var result: results)
+      pose = photonPoseEstimator.update(result);
+  }
+}
